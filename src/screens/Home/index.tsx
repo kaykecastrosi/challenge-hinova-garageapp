fe3de0d5e0fdced0a5d30a4e5835a993f027e4d6
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-22 20:39:46 -0300

    created a function to test the api every time the screen opens

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Container, Title } from "./styles";
+import api from "../../services/api";
 
 export default function Home() {
+  useEffect(() => {
+    api.get("/oficina").then((r) => console.log(r));
+  }, []);
   return (
     <Container>
       <Title>Open up App.tsx to start working on your app!</Title>
