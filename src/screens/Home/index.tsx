c23ad617fb48be74b8bc607ba4e5259b9a1085a2
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-22 20:15:30 -0300

    created an default home component and it styles with styled-componentes

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Container, Title } from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Title>Open up App.tsx to start working on your app!</Title>
+      <StatusBar style="auto" />
+    </Container>
+  );
+}
