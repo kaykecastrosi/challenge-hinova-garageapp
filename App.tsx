4e18171090afd73bb40cdeb0a13d47de80069ca3
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-23 21:28:33 -0300

    imported fonts to the app

--- App.tsx
+++ App.tsx
@@ -1,5 +1,22 @@
 import Routes from "./src/routes";
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return <Routes />;
 }
