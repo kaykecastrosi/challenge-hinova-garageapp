3caa8682f38e467646aba353f196b2818b79f6cc
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-24 20:21:51 -0300

    added navigation function to the screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,15 +2,17 @@ import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Header, Container, Title } from "./styles";
 import api from "../../services/api";
+import { useNavigation } from "@react-navigation/native";
 
 import Card from "../../components/Card";
 
 export default function Home() {
   useEffect(() => {
-    api
-      .get("/oficina?codigoAssociacao=601")
-      .then((r) => console.log(r.data.ListaOficinas));
+    api.get("/oficina?codigoAssociacao=601").then((r) => console.log(r.data));
   }, []);
+
+  const navigation = useNavigation();
+
   return (
     <Container>
       <Header>
@@ -19,10 +21,12 @@ export default function Home() {
       <Card
         title="Consultar oficinas"
         description="Verifique as oficinas disponíveis e tenha acesso à detalhes como e-mail, endereço, telefone, etc."
+        onPress={() => {}}
       />
       <Card
         title="Indicar um amigo"
         description="Verifique as oficinas disponíveis e tenha acesso à detalhes como e-mail, endereço, telefone, etc."
+        onPress={() => {}}
       />
       <StatusBar style="auto" />
     </Container>
