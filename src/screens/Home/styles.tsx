58004e3c1d7034d23a27bf26ee15a4925ba1ac38
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-23 21:29:11 -0300

    created styles for the card component

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,10 +1,23 @@
 import styled from "styled-components/native";
+import { StatusBar } from "react-native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #fff;
+  background-color: #fafafa;
   align-items: center;
-  justify-content: center;
+  padding-top: ${StatusBar.currentHeight}px;
 `;
 
-export const Title = styled.Text``;
+export const Header = styled.View`
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+  padding-left: 20px;
+  padding-top: 40px;
+`;
+
+export const Title = styled.Text`
+  color: #000;
+  font-size: 32px;
+  font-family: "Inter_600SemiBold";
+`;
