fcc4db967d1a173eea4a4e431b974e866c3d0fb1
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-24 20:21:06 -0300

    implemented an mockAPI for testing

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV == "development"
+    ? "https://637ff55c8efcfcedacfa8bb5.mockapi.io/api"
+    : "http://app.hinovamobile.com.br/ProvaConhecimentoWebApi/api";
+
 const api = axios.create({
-  baseURL: "http://app.hinovamobile.com.br/ProvaConhecimentoWebApi/api",
+  baseURL: baseURL,
 });
 
 export default api;
