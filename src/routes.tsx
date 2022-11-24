cf9ca84ae9582c2f6fdca5179aa5111836b6f274
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-23 21:28:19 -0300

    removed header from the stack navigator

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,7 +9,7 @@ const Stack = createNativeStackNavigator();
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
