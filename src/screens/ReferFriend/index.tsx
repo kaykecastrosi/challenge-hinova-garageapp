93b243f31225a93a6f63c85ed7fc7de31c9d71c4
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-28 20:55:52 -0300

    created an ReferFriend screen

---
+++ src/screens/ReferFriend/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Header,
+  HeaderOptions,
+  Container,
+  Title,
+  LoadingIndicator,
+} from "./styles";
+import api from "../../services/api";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function QueryGarages() {
+  const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderOptions>
+          <AntDesign
+            onPress={() => navigation.goBack()}
+            style={{ marginRight: 2 }}
+            name="arrowleft"
+            size={32}
+            color="black"
+          />
+          <AntDesign
+            onPress={() => navigation.navigate("Home")}
+            style={{ marginRight: 2 }}
+            name="home"
+            size={32}
+            color="black"
+          />
+        </HeaderOptions>
+        <Title>Indicar um amigo</Title>
+      </Header>
+      {loading ? <LoadingIndicator size={64} color="black" /> : <></>}
+      <StatusBar style="auto" />
+    </Container>
+  );
+}
