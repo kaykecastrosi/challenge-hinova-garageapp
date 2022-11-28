9c762c0b99e95b109e1fe3a630ce9524d07af914
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-28 19:50:50 -0300

    changed baseURL to unique URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV == "development"
-    ? "https://637ff55c8efcfcedacfa8bb5.mockapi.io/api"
-    : "http://app.hinovamobile.com.br/ProvaConhecimentoWebApi/api";
+const baseURL = "http://app.hinovamobile.com.br/ProvaConhecimentoWebApi/api";
 
 const api = axios.create({
   baseURL: baseURL,
