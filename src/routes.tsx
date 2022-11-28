ad433eb44f7c6844079235edfb97a6efa6a9c517
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-28 20:57:43 -0300

    added all new screens to the routes and added transition animations

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,14 +3,39 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./screens/Home";
+import QueryGaragesScreen from "./screens/QueryGarages";
+import GarageDetailsScreen from "./screens/GarageDetails";
+import ReferFriendScreen from "./screens/ReferFriend";
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen
+          name="Home"
+          options={{ animation: "slide_from_right" }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="QueryGarages"
+          options={{ animation: "slide_from_right" }}
+          component={QueryGaragesScreen}
+        />
+        <Stack.Screen
+          name="ReferFriend"
+          options={{ animation: "slide_from_right" }}
+          component={ReferFriendScreen}
+        />
+        <Stack.Screen
+          name="GarageDetails"
+          options={{ animation: "slide_from_right" }}
+          component={GarageDetailsScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
