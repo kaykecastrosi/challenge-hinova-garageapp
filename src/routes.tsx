c913d566a67dd2fe7bafd4c396827bb166101a1f
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-29 15:00:27 -0300

    added success screen to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import HomeScreen from "./screens/Home";
 import QueryGaragesScreen from "./screens/QueryGarages";
 import GarageDetailsScreen from "./screens/GarageDetails";
 import ReferFriendScreen from "./screens/ReferFriend";
+import SuccessScreen from "./screens/Success";
 
 const Stack = createNativeStackNavigator();
 
@@ -35,6 +36,11 @@ export default function Routes() {
           name="GarageDetails"
           options={{ animation: "slide_from_right" }}
           component={GarageDetailsScreen}
+        />
+        <Stack.Screen
+          name="Success"
+          options={{ animation: "slide_from_right" }}
+          component={SuccessScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
