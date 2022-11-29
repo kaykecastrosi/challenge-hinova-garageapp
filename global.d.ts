58e427d3e638fa97840e77626d6a03c4e01b8e1f
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-28 22:02:46 -0300

    created Garage type and GarageProps type

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,36 @@
 import type { NavigatorScreenParams } from "@react-navigation/native";
+
+export interface IGarageItem {
+  Id: number;
+  Nome: string | null;
+  Descricao: string | null;
+  DescricaoCurta: string | null;
+  Endereço: string | null;
+  Latitude: string | null;
+  Longitude: string | null;
+  Foto: string | null;
+  AvaliacaoUsuario: number | null;
+  CodigoAssociacao: number;
+  Email: string | null;
+  Telefone1: string | null;
+  Telefone2: string | null;
+  Ativo: boolean;
+}
+
+export type IGarageDetailsProps = {
+  params: {
+    item: IGarageItem;
+  };
+};
+
 declare global {
   namespace ReactNavigation {
     interface RootParamList {
       Home: NavigatorScreenParams;
       QueryGarages: NavigatorScreenParams;
-      GarageDetails: NavigatorScreenParams;
+      GarageDetails: {
+        item: IGarageItem;
+      };
       ReferFriend: NavigatorScreenParams;
     }
   }
